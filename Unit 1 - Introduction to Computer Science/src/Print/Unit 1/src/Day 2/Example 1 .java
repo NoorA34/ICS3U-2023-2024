0dbe279ab541d1a990a19d4578e437d2db6b58e9
NoorA34 <nali@bayviewglen.ca>
2023-09-18 08:59:30 -0400

    n

---
+++ Unit 1 - Introduction to Computer Science/src/Print/Unit 1/src/Day 2/Example 1 .java
@@ -0,0 +1,21 @@
+package Day 2;
+
+public class Example1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Question: What is the closest planet to the sun?");
+         System.out.println("A) Jupiter");
+          System.out.println("B) Earth");
+          System.out.println("C) Mercury");
+          System.out.println("D) Saturn");
+           System.out.println("Correct Answer: C) Mercury");
+            System.out.println("");
+             System.out.println("Presentation: Exploring Huron-Wendat Heritage");
+             System.out.println("Aspects of Huron-Wendat Culture:");
+             System.out.println(" - Traditional Practices:");
+             System.out.println(" - Art and Craftsmanship :");
+             System.out.println(" - Language and Storytelling");
+             System.out.println(" - Community Traditions");
+             System.out.println("Date: Wednesday, November 15, 2023");
+             System.out.println("Duration: 60 minutes");
+}
+}
---
+++ Unit 1 - Introduction to Computer Science/src/Print/Unit 1/src/Day 3/Example1.java
@@ -0,0 +1,7 @@
+
+public class Example1 {
+public static void main (String[]args){
+    int num1 = (4);
+    int num2 = (8);
+}
+}
